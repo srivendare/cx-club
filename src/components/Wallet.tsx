170d6706c41c9f32993067bfe3593a7ac171168a
Toy Wang <toy.haagend.wang@gmail.com>
2022-09-01 07:49:34 +0800

    refactored request-wallet and wallet button onpress behaviour

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { FaWallet } from "react-icons/fa";
-import { Box } from '@chakra-ui/react';
+import { Box, Button, Flex } from '@chakra-ui/react';
 import { MetaMaskInpageProvider } from "@metamask/providers";
-
-import { connectWallet, getCurrentWalletConnected } from "../utils/wallet";
 
 
 declare global {
@@ -14,7 +12,51 @@ declare global {
 }
 
 const Wallet: React.FC = () => {
+
+    // -- Models
+
     const [walletAddress, setWallet] = useState("");
+
+
+    // -- Controllers
+
+    const connectWallet = async () => {
+        if (window.ethereum) {
+            let addr = "";
+            await window.ethereum.request({
+                method: 'wallet_requestPermissions',
+                params: [{ eth_accounts: {} }],
+            }).then((permissions: any) => {
+                addr = permissions[0].caveats[0].value[0];
+            }).catch((err) => {
+                console.error(err);
+                // TODO failsafe measure 1: if there exists previous auth
+            });
+            return addr;
+        } else {
+            console.log("MetaMask not installed");
+            return "";
+        }
+    };
+    
+    const getCurrentWalletConnected = async () => {
+        if (window.ethereum) {
+            const addressArray: any = await window.ethereum.request({
+                method: "eth_accounts"
+            });
+            if (addressArray.length > 0) {
+                return addressArray[0];
+            } else {
+                return "";
+            }
+        } else {
+            console.log("MetaMask not installed");
+            return "";
+        }
+    };
+
+
+    // -- Views
 
     useEffect(() => {
         async function fetchWallet() {
@@ -26,7 +68,8 @@ const Wallet: React.FC = () => {
         addWalletListener();
     }, []);
 
-    const connectWalletPressed = async () => {
+    const onWalletPressed = async () => {
+        if (walletAddress.length > 0) return;  // failsafe measure 2
         const walletResponse = await connectWallet();
         setWallet(walletResponse);
         // TODO read err message
@@ -47,10 +90,23 @@ const Wallet: React.FC = () => {
     }
 
     return (
-        <button onClick={connectWalletPressed}>
-            <Box as={FaWallet} size="25px"
-                color={walletAddress.length > 0 ? "green.400" : "grey.200"} />
-        </button>
+        <Button
+            onClick={onWalletPressed}
+            disabled={walletAddress.length > 0}
+            background="transparent"
+        >
+            <Flex 
+                padding='6px 8px'
+                color={walletAddress.length > 0 ? "green.400" : "grey.200"}
+            >
+                {walletAddress.length > 0 ? "Connected" : "Connect your"}
+            </Flex>
+            <Box
+                as={FaWallet}
+                size="25px"
+                color={walletAddress.length > 0 ? "green.400" : "grey.200"}
+            />
+        </Button>
     );
 };
 export default Wallet;
