5c1c7f07d7eac99ad004d58f6a1396ad8b1deb5f
Toy Wang <toy.haagend.wang@gmail.com>
2022-09-09 15:34:47 +0800

    build debug on posts

--- src/components/PostInput.tsx
+++ src/components/PostInput.tsx
@@ -9,22 +9,22 @@ const PostInput: React.FC<PostInputProps> = () => {
 
     return (
         <Box border='1px solid white' mt={2}
-            borderRadius={15} align='center' >
+            borderRadius={15} alignContent='center'>
 
             <Flex w='70%' direction='column' mb={5}>
-                <Text textAlign='left' p={2, 2} fontWeight='bold' fontSize='xl'> Create a Post </Text>
-                <Text textAlign='left' p={2, 2}> Topic </Text>
+                <Text textAlign='left' p={2} fontWeight='bold' fontSize='xl'> Create a Post </Text>
+                <Text textAlign='left' p={2}> Topic </Text>
                 <Input placeholder='Topic'
                     border='1px solid gray'
 
                     borderRadius={7.5} />
-                <Text textAlign='left' p={2, 2}> Commmunity </Text>
+                <Text textAlign='left' p={2}> Commmunity </Text>
                 <Select placeholder='Tag' mt={2}>
                     <option value='option1'>Option 1</option>
                     <option value='option2'>Option 2</option>
                     <option value='option3'>Option 3</option>
                 </Select>
-                <Text textAlign='left' p={2, 2}> Content </Text>
+                <Text textAlign='left' p={2}> Content </Text>
                 <Textarea
                     placeholder='Here is a sample placeholder'
                     border='1px solid gray'
@@ -32,7 +32,7 @@ const PostInput: React.FC<PostInputProps> = () => {
                 />
 
                 <Button type='submit' width='full' color='gray.700' mt={3}>Submit</Button>
-                <Button type='' width='full' color='gray.700' mt={2} >Reset</Button>
+                <Button width='full' color='gray.700' mt={2} >Reset</Button>
             </Flex>
         </Box >
     )
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,7 +1,6 @@
-import { Box, Flex, Text, Image, useColorModeValue } from '@chakra-ui/react';
+import { Box, Text, Image, useColorModeValue } from '@chakra-ui/react';
 import Link from 'next/link';
 import React from 'react';
-import { FaRegCommentDots, FaShareSquare } from 'react-icons/fa';
 
 import { Thread } from "../lib/content";
 
@@ -10,7 +9,17 @@ type ThumbnailProps = {
     src: string | null;
 };
 const Thumbnail: React.FC<ThumbnailProps> = ({ src }) => (
-    src ? <Image width='150px' height='100px' src={src} position="relative" color="blue.500" /> : <></>
+    src ? 
+        <Image 
+            borderRadius='lg' 
+            width={{ md: 40 }}
+            height='100px' 
+            src={src} 
+            position="relative" 
+            color="blue.500"
+        /> 
+    : 
+        <></>
 );
 
 
@@ -22,26 +31,19 @@ export const PostItem: React.FC<PostItemProps> = ({ thread, author }) => {
 
     return (
         <Link href={"/posts/" + thread.threadId}>
-            <Box p={4} display={{ md: 'flex' }} border='1px solid gray' borderRadius={15} mt={3}>
+            <Box p={4} display={{ md: 'flex' }} border='1px solid gray' borderRadius={15} mt={3} 
+                _hover={{bg: useColorModeValue('gray.300', 'gray.300')}}
+            >
                 <Box flexShrink={0}>
-                    <Image
-                        borderRadius='lg'
-                        width={{ md: 40 }}
-                        src={thread.thumbnail}
-                        alt='Woman paying for a purchase'
-                    />
+                    <Thumbnail src={thread.thumbnail} />
                 </Box>
                 <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
-                    <Link
-                        mt={1}
-                        display='block'
-                        fontWeight='bold'
-                        fontSize='lg'
+                    <Box mt={1} display='block'
+                        fontWeight='bold' fontSize='lg'
                         lineHeight='normal'
-                        href='#'
                     >
                         {thread.title}
-                    </Link>
+                    </Box>
                     <Text mt={2} fontSize='sm' color='gray.500'>
                         {thread.content}
                     </Text>
